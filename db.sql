d0c70a9202f49a9ad9a46cb57753db45b802af6c
Bjanette <farid_shokirov@mail.ru>
2023-05-12 16:41:50 +0300

    wind

--- db.sql
+++ db.sql
@@ -3,7 +3,7 @@ CREATE TABLE orders (
                         cour_id BIGINT REFERENCES couriers (courier_id) DEFAULT NULL,
                         weight FLOAT NOT NULL,
                         regions INT NOT NULL,
-                        delivery_hours TEXT[] NOT NULL,
+                        delivery_hours VARCHAR[] NOT NULL,
                         cost INT NOT NULL,
                         completed_time TIMESTAMPTZ DEFAULT NULL);
 
